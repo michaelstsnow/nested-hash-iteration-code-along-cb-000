b971af06777a6ae489bf2e8ae871dbffeadd4c5c
michaelstsnow <michael.st.snow@gmail.com>
2018-11-07 13:23:51 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,11 +15,6 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-  contacts.each do |contact, contact_data|
-    if data == :favorite_ice_cream_flavors data.each do |attribute,flavors|
-      if flavors.include?("strawberry")
-        contacts[contact][data].delete("strawberry")
-      else
-      end
+  contacts.each do |contact, contact_data_hash|
   end
 end
